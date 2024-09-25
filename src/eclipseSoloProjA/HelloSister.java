be96555c096b1e434e8e6e1770b79dc22de66fa9
YadeniEMU <yeba@emich.edu>
2024-09-25 14:46:27 -0400

    Did some nifty stuff

--- src/eclipseSoloProjA/HelloSister.java
+++ src/eclipseSoloProjA/HelloSister.java
@@ -4,6 +4,8 @@ public class HelloSister {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hiiiiiii");
+		System.out.println("Heyyyyyy");
 
 	}
 
